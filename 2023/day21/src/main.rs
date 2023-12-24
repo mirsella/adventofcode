c9f0a6ad9e3aff1c39ea686294e0c51feadbac8d
mirsella <mirsella@protonmail.com>
2023-12-24 15:32:09 +0100

    improve day21

--- 2023/day21/src/main.rs
+++ 2023/day21/src/main.rs
@@ -2,8 +2,8 @@ use pathfinding::{directed::bfs::bfs_reach, matrix::Matrix};
 use std::collections::HashSet;
 
 fn part1(input: &str, steps: usize) -> usize {
-    let matrix = Matrix::from_rows(input.lines().map(str::chars)).unwrap();
-    let start = matrix
+    let grid = Matrix::from_rows(input.lines().map(str::chars)).unwrap();
+    let start = grid
         .iter()
         .enumerate()
         .find_map(|(r, c)| c.iter().position(|c| *c == 'S').map(|c| (r, c)))
@@ -13,9 +13,8 @@ fn part1(input: &str, steps: usize) -> usize {
         if steps == 0 {
             return vec![];
         }
-        matrix
-            .neighbours(pos, false)
-            .filter(|pos| matrix.get(*pos) == Some(&'.') || matrix.get(*pos) == Some(&'S'))
+        grid.neighbours(pos, false)
+            .filter(|pos| grid.get(*pos) == Some(&'.') || grid.get(*pos) == Some(&'S'))
             .map(|pos| (pos, steps - 1))
             .collect::<Vec<_>>()
     });
@@ -23,51 +22,36 @@ fn part1(input: &str, steps: usize) -> usize {
 }
 
 fn part2(input: &str, steps: usize) -> usize {
-    let matrix = Matrix::from_rows(input.lines().map(str::chars)).unwrap();
-    let start = matrix
-        .iter()
-        .enumerate()
-        .find_map(|(r, c)| {
-            c.iter()
-                .position(|c| *c == 'S')
-                .map(|c| (r as isize, c as isize))
-        })
+    let grid = Matrix::from_rows(input.lines().map(str::chars)).unwrap();
+    let start = grid
+        .items()
+        .find_map(|(pos, c)| (*c == 'S').then_some(pos))
         .unwrap();
-    let neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)];
     let mut visited = HashSet::new();
-    visited.insert(start);
+    visited.insert((start.0 as isize, start.1 as isize));
     let mut values = [0; 3];
-    for step in 0..steps {
-        let mut set = HashSet::new();
-        for (x, y) in &visited {
+    for step in 1..=steps {
+        let mut set = HashSet::with_capacity(4 * visited.len());
+        for &(x, y) in &visited {
             set.extend(
-                neighbors
+                [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
                     .iter()
-                    .map(|(dx, dy)| (*x + dx, *y + dy))
-                    .filter(|(x, y)| {
-                        matrix
-                            .get((
-                                (x.rem_euclid(matrix.columns as isize)) as usize,
-                                (y.rem_euclid(matrix.rows as isize)) as usize,
-                            ))
-                            .expect("should always be inbound")
-                            != &'#'
-                    }),
+                    .filter(|(x, y)| grid[grid.constrain((*x, *y))] != '#'),
             );
         }
-        if step % matrix.columns == steps % matrix.columns {
-            let index = step / matrix.columns;
+        visited = set;
+        if step % grid.columns == steps % grid.columns {
+            let index = step / grid.columns;
             values[index] = visited.len();
             if index == values.len() - 1 {
                 break;
             }
         }
-        visited = set;
     }
     let b0 = values[0];
     let b1 = values[1] - values[0];
     let b2 = values[2] - values[1];
-    let n = steps / matrix.columns;
+    let n = steps / grid.columns;
     b0 + b1 * n + (n * (n - 1) / 2) * (b2 - b1)
 }
 
@@ -103,28 +87,4 @@ mod tests {
     fn part2_real() {
         assert_eq!(super::part2(REALINPUT, 26501365), 605247138198755);
     }
-    // #[test]
-    // fn part2_10() {
-    //     assert_eq!(super::part2(INPUT, 10), 50);
-    // }
-    // #[test]
-    // fn part2_50() {
-    //     assert_eq!(super::part2(INPUT, 50), 1594);
-    // }
-    // #[test]
-    // fn part2_100() {
-    //     assert_eq!(super::part2(INPUT, 100), 6536);
-    // }
-    // #[test]
-    // fn part2_500() {
-    //     assert_eq!(super::part2(INPUT, 500), 167004);
-    // }
-    // #[test]
-    // fn part2_1000() {
-    //     assert_eq!(super::part2(INPUT, 1000), 668697);
-    // }
-    // #[test]
-    // fn part2_5000() {
-    //     assert_eq!(super::part2(INPUT, 5000), 16733044);
-    // }
 }
